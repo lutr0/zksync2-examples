c65b4baf0e7a5ce4d194ffd62949f6bb36ce0ffc
Lutro <76906369+lutr0@users.noreply.github.com>
2024-03-29 17:59:50 +0100

    wip: zyfi implementation

--- python/15_use_paymaster.py
+++ python/15_use_paymaster.py
@@ -5,6 +5,8 @@ from pathlib import Path
 from eth_account import Account
 from eth_account.signers.local import LocalAccount
 from eth_typing import HexStr
+import requests
+
 
 from zksync2.core.types import EthBlockParams, PaymasterParams
 from zksync2.manage_contracts.contract_encoder_base import ContractEncoder, JsonConfiguration
@@ -40,7 +42,6 @@ if __name__ == "__main__":
     # Crown token than can be minted for free
     token_address = zk_web3.to_checksum_address("0x927488F48ffbc32112F1fF721759649A89721F8F")
     # Paymaster for Crown token
-    paymaster_address = zk_web3.to_checksum_address("0x13D0D8550769f59aa241a41897D4859c87f7Dd46")
 
     # Provide a compiled JSON source contract
     contract_path = Path("../solidity/custom_paymaster/token/build/Token.json")
@@ -72,87 +73,54 @@ if __name__ == "__main__":
     balance = token_contract.functions.balanceOf(account.address).call()
     print(f"Crown token balance after mint: {balance}")
 
-    # SEND SOME ETH TO PAYMASTER (so it can pay fee with ETH)
-
-    chain_id = zk_web3.zksync.chain_id
-    gas_price = zk_web3.zksync.gas_price
-    signer = PrivateKeyEthSigner(account, chain_id)
-
-    tx_func_call = TxFunctionCall(
-        chain_id=zk_web3.zksync.chain_id,
-        nonce=zk_web3.zksync.get_transaction_count(account.address, EthBlockParams.LATEST.value),
-        from_=account.address,
-        to=paymaster_address,
-        value=zk_web3.to_wei(1, "ether"),
-        data=HexStr("0x"),
-        gas_limit=0,  # Unknown at this state, estimation is done in next step
-        gas_price=gas_price,
-        max_priority_fee_per_gas=100_000_000,
-    )
-
-    # ZkSync transaction gas estimation
-    estimate_gas = zk_web3.zksync.eth_estimate_gas(tx_func_call.tx)
-    print(f"Fee for transaction is: {estimate_gas * gas_price}")
-
-    # Convert transaction to EIP-712 format
-    tx_712 = tx_func_call.tx712(estimate_gas)
-
-    # Sign message & encode it
-    signed_message = signer.sign_typed_data(tx_712.to_eip712_struct())
-
-    # Encode signed message
-    msg = tx_712.encode(signed_message)
-
-    # Transfer ETH
-    tx_hash = zk_web3.zksync.send_raw_transaction(msg)
-    print(f"Transaction hash is : {tx_hash.hex()}")
-
-    # Wait for transaction to be included in a block
-    tx_receipt = zk_web3.zksync.wait_for_transaction_receipt(
-        tx_hash, timeout=240, poll_latency=0.5
-    )
-    print(f"Tx status: {tx_receipt['status']}")
-
+    
     # USE PAYMASTER TO PAY MINT TRANSACTION WITH CROWN TOKEN
 
-    print(f"Paymaster balance before mint: "
-          f"{zk_web3.zksync.get_balance(account.address, EthBlockParams.LATEST.value)}")
-    print(f"User's Crown token balance before mint: {token_contract.functions.balanceOf(account.address).call()}")
-    print(f"Paymaster balance before mint: "
-          f"{zk_web3.zksync.get_balance(paymaster_address, EthBlockParams.LATEST.value)}")
-    print(f"Paymaster Crown token balance before mint: {token_contract.functions.balanceOf(paymaster_address).call()}")
-
     # Use the paymaster to pay mint transaction with token
+    url = 'https://api.zyfi.org/api/erc20_paymaster/v1'
+
     calladata = token_contract.encodeABI(fn_name="mint", args=[account.address, 7])
 
-    # Create paymaster parameters
-    paymaster_params = PaymasterParams(**{
-        "paymaster": paymaster_address,
-        "paymaster_input": zk_web3.to_bytes(
-            hexstr=PaymasterFlowEncoder(zk_web3).encode_approval_based(token_address, 1, b''))
-    })
+payload = {
+    "feeTokenAddress": token_contract.address,
+    "isTestnet": True,
+    "txData": {
+        "from": account.address,
+        "to": "token_address",
+        "data": calladata
+    }
+}
+# Sending a POST request to the API endpoint
+response = requests.post(url, json=payload)
+response_data = response.json()
 
-    tx_func_call = TxFunctionCall(
-        chain_id=zk_web3.zksync.chain_id,
-        nonce=zk_web3.zksync.get_transaction_count(account.address, EthBlockParams.LATEST.value),
-        from_=account.address,
-        to=token_address,
-        data=calladata,
-        gas_limit=0,  # Unknown at this state, estimation is done in next step
-        gas_price=gas_price,
-        max_priority_fee_per_gas=100_000_000,
-        paymaster_params=paymaster_params
-    )
-    a = tx_func_call.tx;
-    # ZkSync transaction gas estimation
-    estimate_gas = zk_web3.zksync.eth_estimate_gas(tx_func_call.tx)
-    print(f"Fee for transaction is: {estimate_gas * gas_price}")
+# Extracting data from the response
+tx_data = response_data['txData']
 
-    # Convert transaction to EIP-712 format
-    tx_712 = tx_func_call.tx712(estimate_gas)
+# Setting up parameters based on the response
+chain_id = tx_data['chainId']
+from_address = tx_data['from']
+to_address = tx_data['to']
+data = tx_data['data']
+paymaster_params = tx_data['customData']['paymasterParams']
+gas_limit = tx_data['gasLimit']
+max_fee_per_gas = tx_data['maxFeePerGas']
+
+
+  tx_func_call = TxFunctionCall(
+    chain_id=chain_id,
+    nonce=zk_web3.zksync.get_transaction_count(account.address, EthBlockParams.LATEST.value),
+    from_=from_address,
+    to=to_address,
+    data=data,
+    gas_limit=gas_limit,  # Unknown at this state, estimation is done in next step
+    gas_price=max_fee_per_gas,
+    max_priority_fee_per_gas=0,
+    paymaster_params=paymaster_params
+)
 
     # Sign message & encode it
-    signed_message = signer.sign_typed_data(tx_712.to_eip712_struct())
+    signed_message = signer.sign_typed_data(tx_func_call.to_eip712_struct())
 
     # Encode signed message
     msg = tx_712.encode(signed_message)
